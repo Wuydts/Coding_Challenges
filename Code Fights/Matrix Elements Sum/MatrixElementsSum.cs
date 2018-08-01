ad9fb23f258db4051a517ff7700ed0ff7657565d
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-08-01 14:49:31 -0500

    Create MatrixElementsSum.cs

---
+++ Code Fights/Matrix Elements Sum/MatrixElementsSum.cs
@@ -0,0 +1,21 @@
+int matrixElementsSum(int[][] matrix) {
+
+
+
+     int i, j;
+     int count = 0;
+     for (j = 0; j < matrix[0].Length; j++)
+     {
+    	  for (i = 0; i < matrix.Length; i++)
+          {
+    	     if (matrix[i][j] == 0) 
+              break;
+         
+    	        count += matrix[i][j];
+          }
+     }
+    	    return count;
+}
+    	
+    	
+    	
